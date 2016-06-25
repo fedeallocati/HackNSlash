413c242a0039120a3d2b3dba36a265ced447623a
Federico Allocati <fede.allocati@gmail.com>
2016-06-25 14:26:10 -0300

    Complete tutorial 8

---
+++ Assets/Scripts/Targetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Targetting : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Transform selectedTarget;
+
+    // Use this for initialization
+    public void Start ()
+	{
+	    targets = GameObject.FindGameObjectsWithTag("Enemy").Select(enemy => enemy.transform).ToList();
+        selectedTarget = null;
+
+	}
+
+    private void TargetEnemy()
+    {
+        selectedTarget = targets.First();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            TargetEnemy();
+        }
+    }
+}
