cc145293b82d967a069217e0aa4841c9ebf315ff
Federico Allocati <fede.allocati@gmail.com>
2016-06-25 01:03:22 -0300

    Create Unity Project and VS 2015 Solution with Tutorial 01

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public int maxHealth = 100;
+    public int currHealth = 100;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+        GUI.Box(new Rect(10, 10, Screen.width / 2 / (maxHealth / currHealth), 20), currHealth + "/" + maxHealth);
+    }
+}
